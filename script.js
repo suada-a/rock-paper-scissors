ae0c42fc9fe0f12a26dd4eb6c0d41322d8f30098
Suada Abdurahim <36940887+suada-a@users.noreply.github.com>
2022-01-06 17:31:06 -0500

    Add play functionality

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-function computerPlayer(choices) {
-    return choices[Math.floor(Math.random()*choices.length)];
+function computerPlayer() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-const choices = ["Rock", "Paper", "Scissors"];
+function playRound(computerSelection, playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
 
-console.log(computerPlayer(choices));
+    if (computerSelection == "rock") {
+        if (playerSelection == "paper") {
+            return "You Win! Paper beats Rock";
+        } else if (playerSelection == "scissors") {
+            return "You Lose! Rock beats Scissors";
+        } else {
+            return "Tie";
+        }
+    } else if (computerSelection == "paper") {
+        if (playerSelection == "rock") {
+            return "You Lose! Paper beats Rock";
+        } else if (playerSelection == "scissors") {
+            return "You Win! Scissors beat Paper";
+        } else {
+            return "Tie";
+        } 
+    } else if (computerSelection == "scissors" && playerSelection == "") {
+        if (playerSelection == "rock") {
+            return "You Win! Rock beats Scissors";
+        } else if (playerSelection == "paper") {
+            return "You Lose! Scissors beats Rock";
+        } else {
+            return "Tie";
+        }
+    }
+}
+
+const computerSelection = computerPlayer();
+const playerSelection = prompt("Enter your selection: ");
+
+console.log(playRound(playerSelection, computerSelection));
